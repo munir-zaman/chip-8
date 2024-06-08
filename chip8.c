006b4a5ce99b705d4a3e42e5e067869628c145c0
Munir Zaman <munir.zaman2k22@outlook.com>
2024-06-08 14:46:32 +0600

    load_rom and tetris.ch8

--- chip8.c
+++ chip8.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 #include<stdint.h>
+#include<stdlib.h>
 
-struct Chip8 {
+const unsigned int START_ADDRESS = 0x200;
+
+typedef struct Chip8 {
     // 4kb of memory
     uint8_t memory[4096];
     // 16 8-bit registers (V0 - VF)
@@ -21,4 +24,39 @@ struct Chip8 {
     uint32_t display[64][32];
     // 16bit opcode
     uint16_t opcode;
-};
+} Chip8;
+
+Chip8 chip8;
+
+int load_rom(char *filename) {
+    FILE *fp;
+    fp = fopen(filename, "rb");
+
+    if (fp == NULL) {
+        printf("Error : failed to load file \'%s\'\n", filename);
+        return -1;
+    }
+
+    // calculate size of file -> f_size
+    fseek(fp, 0L, SEEK_END);
+    long int f_size = ftell(fp);
+
+    // reset position
+    fseek(fp, 0L, SEEK_SET);
+
+    // load to memory
+    unsigned char x;
+    for (int i = 0; ((x = fgetc(fp)) != EOF); i++) {
+        chip8.memory[START_ADDRESS + i] = x;
+    }
+
+    // close and return
+    fclose(fp);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        load_rom(argv[1]);
+    }
+}
