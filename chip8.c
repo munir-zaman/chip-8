fb2eebace3bab2efdbd683adf881876641247e90
Munir Zaman <munir.zaman2k22@outlook.com>
2024-06-10 14:34:16 +0600

    implemented some opcodes :D

--- chip8.c
+++ chip8.c
@@ -76,20 +76,103 @@ void execute_instruction(uint16_t opcode) {
     // last_digit is going to store the last nibble or hex value
     uint8_t last_digit = HEX(opcode, 3);
     if (DEBUG) printf("Opcode Type : %02X\n", last_digit);
+
+    uint16_t nnn = (opcode & 0x0FFF);
+    uint16_t kk = (opcode & 0x00FF);
+    uint16_t x = HEX(opcode, 2);
+    uint16_t y = HEX(opcode, 1);
+    uint16_t n = HEX(opcode, 0);
+
     if (last_digit == 0x0) {
         // 00E0 - CLS
-        if (HEX(opcode, 1) == 0xE && HEX(opcode, 0) == 0x0) {
+        if (opcode == 0x00E0) {
             for (int i = 0; i < 64; i++)
                 for (int j = 0; j < 32; j++)
                     chip8.display[i][j] = DEFAULT_COLOR;
-
-            if (DEBUG) printf("Cleared The Screen");
         }
         // 00EE - RET
-        else if (HEX(opcode, 1) == 0xE && HEX(opcode, 0) == 0xE) {
+        else if (opcode == 0x00EE) {
             chip8.pc = chip8.stack[chip8.sp];
             chip8.sp--;
-            if (DEBUG) printf("Executed RET.. returned to %04X", chip8.pc);
+            chip8.pc += 2; // move to next instruction
+        }
+        // 0nnn ?? 
+        // Not necessary
+    }
+    else if (last_digit == 0x1) {
+        // JP addr
+        chip8.pc = nnn;
+    }
+    else if (last_digit == 0x2) {
+        // CALL addr
+        chip8.sp++;
+        chip8.stack[chip8.sp] = chip8.pc;
+        chip8.pc = nnn;
+    }
+    else if (last_digit == 0x3) {
+        // 3xkk - SE Vx, byte
+        // Skip next instruction if Vx = kk.
+        if (chip8.V[x] == kk) {
+            chip8.pc += 2;
+        }
+    }
+    else if (last_digit == 0x4) {
+        if (chip8.V[x] != kk) {
+            chip8.pc += 2;
+        }
+    }
+    else if (last_digit == 0x5) {
+        if (chip8.V[x] == chip8.V[y]) chip8.pc += 2;
+    }
+    else if (last_digit == 0x6) {
+        chip8.V[x] = kk;
+    }
+    else if (last_digit == 0x7) {
+        chip8.V[x] += kk;
+    }
+    else if (last_digit == 0x8) {
+        if (n == 0x1) {
+            chip8.V[x] |= chip8.V[y];
+        }
+        else if (n == 0x2) {
+            chip8.V[x] &= chip8.V[y];
+        }
+        else if (n == 0x3) {
+            chip8.V[x] ^= chip8.V[y];
+        }
+        else if (n == 0x4) {
+            // check for overflow
+            uint16_t vx = (uint16_t) chip8.V[x];
+            uint16_t vy = (uint16_t) chip8.V[y];
+            uint8_t carry = (vx + vy) > 0xFF;
+            // store vx + vy in vx
+            chip8.V[x] += chip8.V[y];
+            // store carry
+            chip8.V[0xF] = carry;
+        }
+        else if (n == 0x5) {
+            uint8_t borrow = (chip8.V[x] >= chip8.V[y]); 
+            // No borrow if Vx is greater than or equal to Vy
+            chip8.V[x] -= chip8.V[y];
+            chip8.V[0xF] = borrow; // Store the borrow flag in VF
+        }
+        else if (n == 0x6) {
+            // store lsb in vf
+            chip8.V[0xF] = chip8.V[x] & 0x01;
+            // shift right by one
+            chip8.V[x] >>= 1;
+        }
+        else if (n == 0x7) {
+            uint8_t borrow = (chip8.V[y] >= chip8.V[x]); 
+            // No borrow if Vy is greater than or equal to Vx
+            chip8.V[x] = chip8.V[y] - chip8.V[x];
+            chip8.V[0xF] = borrow; // Store the borrow flag in VF
+        }
+        else if (n == 0xE) {
+            // store msb in vf
+            chip8.V[0xF] = chip8.V[x] >> 7;
+            // shift left by one
+            chip8.V[x] <<= 1;
         }
     }
 }
